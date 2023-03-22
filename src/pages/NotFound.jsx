6e5109145ad8748b1efe13a8dff31fce5412a7bb
Anais69 <116172415+Anais69@users.noreply.github.com>
2023-03-22 19:47:26 +0100

    css housing page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,17 +4,19 @@ import '../styles/NotFound.css'
 
 function NotFound() {
   return (
-    <div className="notFound">
+    <div>
       <Header />
-      <div className="notFound_txt">
-        <h1 className="notFound_title">404</h1>
-        <p className="notFound_description">
-          Oups! La page que vous recherchez n'existe pas.
-        </p>
+      <div className="notFound">
+        <div className="notFound_txt">
+          <h1 className="notFound_title">404</h1>
+          <p className="notFound_description">
+            Oups! La page que vous recherchez n'existe pas.
+          </p>
 
-        <Link className="notFound_return_home" to="/Kasa/">
-          Retouner sur la page d'accueil
-        </Link>
+          <Link className="notFound_return_home" to="/Kasa/">
+            Retouner sur la page d'accueil
+          </Link>
+        </div>
       </div>
     </div>
   )
