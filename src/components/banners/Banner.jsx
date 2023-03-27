43ba93ac6886049343923d9736b2b61c4b9d200b
Anais69 <116172415+Anais69@users.noreply.github.com>
2023-03-27 16:14:39 +0200

    .

--- src/components/banners/Banner.jsx
+++ src/components/banners/Banner.jsx
@@ -1,11 +1,13 @@
 import '../../styles/home/Banner.css'
 
 function Banner({ image }) {
-  const text = 'Chez vous, partout et ailleurs'
   return (
     <div className="banner">
       <img className="banner_img" src={image} alt="falaises" />
-      <h1 className="banner_txt">{text}</h1>
+      <h1 className="banner_txt">
+        Chez vous, <br />
+        partout et ailleurs
+      </h1>
     </div>
   )
 }
