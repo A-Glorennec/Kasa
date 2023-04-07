59be667a543155b95369f5e7dd11af321d84d888
Anais69 <116172415+Anais69@users.noreply.github.com>
2023-04-07 19:47:40 +0200

    Modifications après soutenance

--- src/components/outils/Carousel.jsx
+++ src/components/outils/Carousel.jsx
@@ -6,13 +6,16 @@ import arrowLeft from '../../assets/arrow_left.png'
 import '../../styles/housing/Carousel.css'
 
 function Carousel({ slides }) {
+  // Déclare une variable d'état initialisée à 0
   const [current, setCurrent] = useState(0)
-
+  // Fonction  qui incrémente l'image actuelle de 1
+  // Si current atteint la fin de la liste de photos, revient à la première
   const nextSlide = () => {
     setCurrent(current + 1)
     if (current === slides.length - 1) setCurrent(0)
   }
-
+  // Fonction qui décrémente l'image actuelle de 1
+  // Si current est à la première photo, va à la dernière photo
   const previousSlide = () => {
     setCurrent(current - 1)
     if (current === 0) setCurrent(slides.length - 1)
--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,80 +1,74 @@
 import { useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-// données des logements et des hôtes
+// Données
 import data from '../data/data.json'
-// composants
-import Header from '../components/header/Header'
+// Composants
 import Carousel from '../components/outils/Carousel'
 import Collapse from '../components/outils/Collapse'
 import Footer from '../components/Footer'
-// images
+import Header from '../components/header/Header'
+// Images
 import greyStar from '../assets/grey_star.png'
 import redStar from '../assets/red_star.png'
-// styles
+// Styles
 import '../styles/housing/Housing.css'
 
 function Housing() {
-  // 	Déclaration d’une variable d’état en lui donnant une valeur initiale, ici un tableau vide
-  const [slides, setSlides] = useState([])
-  // Extraction de l'id de l'url
-  const houseId = useParams('id').id
-  // On filtre le logement à afficher avec son id
-  const dataCurrentHouse = data.filter((data) => data.id === houseId)
-  // Met à jour slides que lorsque l'id du logement change
-  useEffect(() => {
-    // récupération des données du logement acuel par rapport à son id
-    const dataCurrentHouse = data.filter((data) => data.id === houseId)
-    setSlides(dataCurrentHouse[0].pictures)
-  }, [houseId])
-  // On extrait les données de l'hôte et du logement  et on les stocke
-  const name = dataCurrentHouse[0].host.name.split(' ')
-  const rating = dataCurrentHouse[0].rating
-  const description = dataCurrentHouse[0].description
-  const equipments = dataCurrentHouse[0].equipments
+  // On extrait l'id du logement depuis l'url
+  const { id } = useParams()
+  // On recherche le logement correspondant à l'id
+  const currentHouse = data.find((housing) => housing.id === id)
+  // Si l'id n'est pas trouvé, on redirige
+  if (currentHouse === undefined) {
+    window.location.href = '/error'
+  }
+  // On extrait les données du logement
+  const {
+    host,
+    title,
+    location,
+    tags,
+    description,
+    equipments,
+    rating,
+    pictures: housingPictures,
+  } = currentHouse
+
+  // On extrait les données de l'hôte
+  const { name, picture: hostPicture } = host
+  const [firstName, lastName] = name.split(' ')
+  // On itère sur un tableau de 5 étoiles pour afficher les étoiles en fonction des notes des hôtes
+  const stars = [...Array(5)].map((_, i) => (
+    <img key={i} src={i < rating ? redStar : greyStar} alt="star" />
+  ))
 
   return (
     <>
       <Header />
       <main className="housing">
-        <Carousel slides={slides} />
+        <Carousel slides={housingPictures} />
         <section className="housing_content">
           <div className="housing_info">
-            <h1>{dataCurrentHouse[0].title}</h1>
-            <p>{dataCurrentHouse[0].location}</p>
+            <h1>{title}</h1>
+            <p>{location}</p>
             <div>
-              {dataCurrentHouse[0].tags.map((tag, index) => {
-                return <button key={index}>{tag}</button>
-              })}
+              {tags.map((tag, index) => (
+                <button key={index}>{tag}</button>
+              ))}
             </div>
           </div>
           <div className="housing_host">
             <div className="housing_host_name">
               <div className="host_name">
-                <span>{name[0]}</span>
-                <span>{name[1]}</span>
+                <span>{firstName}</span>
+                <span>{lastName}</span>
               </div>
               <img
                 className="host_picture"
-                src={dataCurrentHouse[0].host.picture}
+                src={hostPicture}
                 alt="hôte du logement"
               />
             </div>
-
-            <div className="host_stars">
-              {(() => {
-                const stars = []
-                for (let i = 1; i <= 5; i++) {
-                  stars.push(
-                    <img
-                      key={i}
-                      src={i <= rating ? redStar : greyStar}
-                      alt="star"
-                    />
-                  )
-                }
-                return stars
-              })()}
-            </div>
+            <div className="host_stars">{stars}</div>
           </div>
         </section>
         <section className="housing_collapse">
